24319f11db7b3c9a059143c2017cc8b89ca96df9
YahyaSulaim <yahyasulaim.p.v@gmail.com>
2024-10-29 00:20:03 +0530

    e

--- app.py
+++ app.py
@@ -11,12 +11,13 @@ def index():
     if request.method == 'POST':
         data = request.form.get('data')
         qr_scale= request.form.get('scale')
-        # logo = request.form.get('logo')
+        logo = request.form.get('logo')
 
         qr_code_path = os.path.join(app.config['UPLOAD_FOLDER'], 'qr_code.png')
         qr = segno.make(data, error='H')
         qr.save(qr_code_path,
                 scale = int(qr_scale)/49)
+        
         return send_file(qr_code_path, mimetype='image/png')
     
     return render_template('index.html')
